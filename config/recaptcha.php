76f3c6b10257d5460449e82cc6d09f6cc96bc6ee
Power Head <p0w3rhead@gmail.com>
2011-05-15 00:56:47 +0200

    modified keys

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'private_key' => '6LevVsQSAAAAAB1-UHIM-WR36hDY-n7NbS5xoFcC',
-	'public_key' => '6LevVsQSAAAAAJfvoaCW4lmMEz_UCCt26xFkKugC',
+    'private_key' => '',
+	'public_key' => '',
 	'server' => 'http://www.google.com/recaptcha/api',
 	'secure_server' => 'https://www.google.com',
 	'verify_server' => 'www.google.com',
